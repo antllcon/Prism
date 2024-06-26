6cd9d76204a93ac0b16f00f8f490c722209489e3
Степан <pokemonivan32@gmail.com>
2024-06-26 09:40:59 +0300

    task-3 Изменены ошибки в коде

--- js/script.js
+++ js/script.js
@@ -1,27 +1,13 @@
-let requestAnimFrame = (function() {
-    return window.requestAnimationFrame    ||
-           window.webkitRequestAnimationFrame ||
-           window.mozRequestAnimationFrame    ||
-           window.oRequestAnimationFrame      ||
-           window.msRequestAnimationFrame     ||
-           function(callback) {
-               window.setTimeout(callback, 1000 / 60);
-           };
-})();
-
 let canvas = document.getElementById("canvas");
 
 const canvasWidth = 400;
 const canvasHeight = 400;
-
-let PLAYER = {
-    pos: [20, 20], // Точка появления игрока
-    size: 10,
-    color: '#00A86B',
-    speed: 200,
-    team: 1, // 1 - Зеленые, 2 - Красные - в будущем
-    live: 1
-};
+const red = "red";
+const green = "#00A86B";
+const gray = "#666";
+const black = "#111";
+const lineSize = 300;
+const startPos = [20, 20];
 
 let GAME = {
     width: canvasWidth,
@@ -29,16 +15,23 @@ let GAME = {
     background: '#333'
 };
 
+let PLAYER = {
+    pos: [20, 20],
+    size: 10,
+    color: black,
+    speed: 200,
+    team: 1, 
+    live: 1
+};
+
 let POINT = {
     pos: [200, 200],
     width: 10,
     height: 10,
-    line: 10,
-    color: '#666',
-    active: true,
-    type: 0, // 0 - point, 1 - line, 2 - cross
-    time: 5000,
-    team: 2
+    type: 0,
+    active: false,
+    team: 0,
+    color: gray
 };
 
 canvas.width = GAME.width;
@@ -46,7 +39,6 @@ canvas.height = GAME.height;
 
 // Инструменты рисования
 let ctx = canvas.getContext("2d");
-let gameTime = 0; // нужен для повышения сложности
 
 function drawBackground() {
     ctx.fillStyle = GAME.background;
@@ -54,45 +46,53 @@ function drawBackground() {
 }
 
 function drawPlayer() {
+    if (PLAYER.live === 1) {
+        if (PLAYER.team === 1) {
+            PLAYER.color = green;
+        }
+        if (PLAYER.team === 2) {
+            PLAYER.color = red;
+        }    
+    } else {
+        PLAYER.color = black
+    }
+    
     ctx.fillStyle = PLAYER.color;
     ctx.fillRect(PLAYER.pos[0], PLAYER.pos[1], PLAYER.size, PLAYER.size);
 }
 
 function drawPoint() {
-    ctx.fillStyle = POINT.color;
-    if (POINT.type === 0) {
+    if (POINT.active) {
+        if (POINT.type === 1) {
+            ctx.fillStyle = POINT.color;
+            ctx.fillRect(POINT.pos[0], POINT.pos[1], POINT.width, POINT.height);
+        }
+        if (POINT.type === 2) {
+
+        }
+    } else {
+        ctx.fillStyle = POINT.color;
         ctx.fillRect(POINT.pos[0], POINT.pos[1], POINT.width, POINT.height);
-    } else if (POINT.type === 1) {
-        ctx.fillRect(POINT.pos[0], POINT.pos[1], POINT.width, POINT.height);
-    } else if (POINT.type === 2) {
-        ctx.beginPath();
-        ctx.moveTo(POINT.pos[0] - 150, POINT.pos[1]);
-        ctx.lineTo(POINT.pos[0] + 150, POINT.pos[1]);
-        ctx.moveTo(POINT.pos[0], POINT.pos[1] - 150);
-        ctx.lineTo(POINT.pos[0], POINT.pos[1] + 150);
-        ctx.strokeStyle = POINT.color;
-        ctx.lineWidth = POINT.line;
-        ctx.stroke();
-        ctx.closePath();
     }
 }
 
 function drawFrame() {
     ctx.clearRect(0, 0, GAME.width, GAME.height);
     drawBackground();
-    drawPlayer();
     drawPoint();
+    drawPlayer();
 }
 
 let lastTime;
-let pointActivatedTime = null;
-let timeExist = 1000;
+let timeExist = 3000;
+let pointActiveTime = null;
 let pointActiveExist = false; 
 
 // Инициализация
 function init() {
     drawBackground();
-    drawPlayer();
+    drawPoint();
+    drawPlayer(); 
     lastTime = Date.now();
     main();
 }
@@ -104,33 +104,18 @@ function main() {
 
     update(dt);
     render();
+
     lastTime = now;
     requestAnimFrame(main);
 }
 
+let gameTime = 0;
+
 function update(dt) {
     gameTime += dt;
     handleInput(dt);
-
-
-    // Проверка времени существования POINT.active
-    if (Date.now() - pointActivatedTime > timeExist) {
-        POINT.active = false;
-        POINT.type = 0;
-        POINT.color = '#666';
-        pointActivatedTime = null;
-        pointActiveExist = false;
-
-        POINT.width = 10;
-        POINT.pos[0] = 200; // нужно вынести в константу
-        POINT.team = 0
-
-    } else {
-        pointActiveExist = true;
-    }
-
-    updateEntitiesCollision();
     checkCollisions();
+    updateEntities();
 }
 
 // Обработка нажатой клавиши
@@ -152,45 +137,9 @@ function handleInput(dt) {
     }
 }
 
-const lineSize = 300;
-
-// Обновление сущностей
-function updateEntitiesCollision() {
-
-    if (PLAYER.pos[0] < POINT.pos[0] + POINT.width &&
-        PLAYER.pos[0] + PLAYER.size > POINT.pos[0] &&
-        PLAYER.pos[1] < POINT.pos[1] + POINT.height &&
-        PLAYER.pos[1] + PLAYER.size > POINT.pos[1]) {
-            
-        POINT.active = true;
-        // В зависимости от текущего type в POINT должно меняться поведение
-        if (POINT.type === 0) {
-            POINT.team = PLAYER.team;
-            POINT.color = PLAYER.color;
-            POINT.type = 1; // Сделать зависимость от номера POINT на карте
-            if (!pointActiveExist) {
-                POINT.width = lineSize;
-                POINT.pos[0] = POINT.pos[0] - POINT.width / 2;
-                pointActivatedTime = Date.now(); // Установка времени активации
-            }
-        } else if (POINT.team === PLAYER.team) {
-            POINT.color = PLAYER.color;
-        } else  if (POINT.team !== PLAYER.team) {
-            PLAYER.live = 0;
-            POINT.color = "red"; // Изменить цвет на красный при столкновении
-        }
-    } else {
-        POINT.color = "#666";
-    }
-
-    if (PLAYER.live === 0) {
-        PLAYER.pos = [20, 20];
-        PLAYER.color = "black";
-    }
-}
-
-function checkCollisions() {
+function checkCollisions(dt) {
     checkPlayerBounds();
+    checkEntitiyBounds();
 }
 
 function checkPlayerBounds() {
@@ -208,11 +157,64 @@ function checkPlayerBounds() {
     }
 }
 
+function checkEntitiyBounds() {
+    if (POINT.pos[0] + POINT.width > PLAYER.pos[0] &&
+        POINT.pos[0] < PLAYER.pos[0] + PLAYER.size &&
+        POINT.pos[1] + POINT.height > PLAYER.pos[1] &&
+        POINT.pos[1] < PLAYER.pos[1] + PLAYER.size) {
+        
+        POINT.active = true;
+    } 
+}
+
+function updateEntities() {
+    if (POINT.active) {
+        if (POINT.team === 0) {
+            POINT.type = 1;
+            POINT.team = PLAYER.team;
+            POINT.width = lineSize;
+            POINT.pos[0] = POINT.pos[0] - POINT.width / 2;
+            pointActiveTime = Date.now();
+        }
+        if (POINT.team === PLAYER.team) {
+            POINT.color = PLAYER.color;
+        } else {
+            PLAYER.live = 0;
+            PLAYER.color = black;
+            PLAYER.pos = startPos;
+            POINT.color = red;
+        }
+        if (Date.now() - pointActiveTime < timeExist) {
+            pointActiveExist = true;
+        } else {
+            POINT.active = false;
+        }
+    } else {  // изменяем состояния POINT в исходные 
+        POINT.pos = [200, 200];
+        POINT.width = 10;
+        POINT.type = 0;
+        POINT.team = 0;
+        POINT.color = gray;
+    }     
+}    
+
 function render() {
     ctx.clearRect(0, 0, GAME.width, GAME.height);
     drawBackground();
     drawPoint();
     drawPlayer();
 }
+
+// Определение requestAnimFrame
+window.requestAnimFrame = (function() {
+    return window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        window.oRequestAnimationFrame ||
+        window.msRequestAnimationFrame ||
+        function(callback) {
+            window.setTimeout(callback, 1000 / 60);
+        };
+})();
 
 init();
