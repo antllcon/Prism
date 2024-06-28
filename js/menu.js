a72f90688e3a42e9924cc34120284aaf76274b27
VladiSLAVE122 <pogorelov7123@gmail.com>
2024-06-28 19:13:00 +0300

    task-7 Создал файл для работы с меню через js

---
+++ js/menu.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const buttonBot = document.getElementById("button-bot");
+    const buttonPlayer = document.getElementById("button-player")
+
+    
+})
