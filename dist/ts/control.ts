91984e7e5642cb8da858241d2d6cbfba7f6d59d2
ChezganovaAnna <tchezganova.aniuta@yandex.ru>
2024-07-03 02:24:22 +0300

    task-22 добавили в гитигнор ненужные файлы перевела проект на ts  появились проблемы с версткой

--- dist/ts/control.ts
+++ dist/ts/control.ts
@@ -35,7 +35,7 @@
     window.addEventListener('blur', function() {
         pressedKeys = {};
     });
-
+//@ts-ignore
     window.input = {
         isDown: function(key) {
             return pressedKeys[key.toUpperCase()];
