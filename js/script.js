9d49f3341fe400ad21a330aa59092d65585fb003
w1ldhoneyp1e <k_yashmetov@mail.ry>
2024-07-04 08:49:46 +0300

    task-33 добавил в условие коллизии границ карты учет размера бота и игрока

--- js/script.js
+++ js/script.js
@@ -622,26 +622,26 @@ function checkCollisions() {
 function checkBorderGameBounds() {
     // Проход через границы поля для ИГРОКА
     if (PLAYER.x < 0) {
-        PLAYER.x = GAME.width;
-    } else if (PLAYER.x > GAME.width) {
+        PLAYER.x = GAME.width - PLAYER.size;
+    } else if (PLAYER.x + PLAYER.size > GAME.width) {
         PLAYER.x = 0;
     }
 
     if (PLAYER.y < 0) {
-        PLAYER.y = GAME.height;
-    } else if (PLAYER.y > GAME.height) {
+        PLAYER.y = GAME.height - PLAYER.size;
+    } else if (PLAYER.y + PLAYER.size > GAME.height) {
         PLAYER.y = 0;
     }
     // Проход через границы поля БОТА
     if (BOT.x < 0) {
-        BOT.x = GAME.width;
-    } else if (BOT.x > GAME.width) {
+        BOT.x = GAME.width - BOT.size;
+    } else if (BOT.x + BOT.size > GAME.width) {
         BOT.x = 0;
     }
 
     if (BOT.y < 0) {
-        BOT.y = GAME.height;
-    } else if (BOT.y > GAME.height) {
+        BOT.y = GAME.height - BOT.size;
+    } else if (BOT.y + BOT.size > GAME.height) {
         BOT.y = 0;
     }
 }
