95a3c990f1776f957d6af59c71d33305d60e64b8
VladiSLAVE122 <pogorelov7123@gmail.com>
2024-07-17 14:28:16 +0300

    task-37 Отрисовка карточек происходит теперь через js и есть возможность добавить карточки бота вместо карточек ожидания (пока очень кривая)

--- js/menu.js
+++ js/menu.js
@@ -5,7 +5,6 @@ const socket = io();
 let globalRoomId;
 
 let centralPartMenu;
-let nameGame;
 let buttonBot;
 let buttonPlayer;
 let button1vs1;
@@ -18,7 +17,6 @@ let buttonPlay;
 let buttonMenu;
 let buttonLeave;
 let inputRoomId;
-let waitingCard;
 
 function loadHTML(filename, callback) {
     let xhr = new XMLHttpRequest();
@@ -36,13 +34,12 @@ function callback(html) {
 function transitionToPage(file) {
     loadHTML(file, (html) => {
         callback(html);
-        if (file === 'menu.html' || file === 'with_bot.html'
-            || file === 'with-player.html') {
+        if (file === 'menu.html' || file === 'with_bot.html' || file === 'with-player.html') {
             loadToMainPageLink();
         }
-        if (file === 'lobby.html') {            
+        if (file === 'lobby.html') {
             let chapterCodeElement = document.querySelector('.chapter__code');
-            chapterCodeElement.textContent = globalRoomId;        
+            chapterCodeElement.textContent = globalRoomId;
         }
 
         initEventListeners();
@@ -51,16 +48,119 @@ function transitionToPage(file) {
 
 function loadCard(templateId) {
     const template = document.getElementById(templateId);
-    const cardContainer = document.getElementById('card-container');
-    const card = template.content.cloneNode(true);
-    cardContainer.appendChild(card);
+    if (template) {
+        const cardContainer = document.getElementById('card-container');
+        if (cardContainer) {
+            const card = template.content.cloneNode(true);
+            cardContainer.appendChild(card);
+        }
+    }
 }
 
-function addBot() {
-    const waitingCard = buttonGetBot.closest('.player');
+function setStyleCard(index) {
+    let cardBackgrounds = document.getElementsByClassName('player');
+    let namePlayers = document.getElementsByClassName('player-count');
+    let nameCharacters =  document.getElementsByClassName('player-name');
+    let navigationArrowsLeft = document.getElementsByClassName('arrow-left');
+    let navigationArrowsRight = document.getElementsByClassName('arrow-right');
+    let buttonsPickBot = document.getElementsByClassName('player-get-bot');
+    let playersPicture = document.getElementsByClassName('player-img');
+
+    if (index < cardBackgrounds.length) {
+        let cardBackground = cardBackgrounds[index];
+        let namePlayer = namePlayers[index];
+        let nameCharacter = nameCharacters[index];
+        let navigationArrowLeft = navigationArrowsLeft[index];
+        let navigationArrowRight = navigationArrowsRight[index];
+        let buttonPickBot = buttonsPickBot[index-1];
+        let playerPicture = playersPicture[index];
+
+        if (index === 0) {
+            cardBackground.style.backgroundColor = "#6000C1";
+            namePlayer.style.color = "#E9D3FF";
+            if (nameCharacter) {
+                nameCharacter.style.color = "#E9D3FF";
+                navigationArrowLeft.style.borderRight = "12px solid #E9D3FF";
+                navigationArrowRight.style.borderLeft = "12px solid #E9D3FF";
+                playerPicture.src = "src/assets/img/kandinsky-download-1719222086261%201.svg";
+            }
+            if (buttonPickBot) {
+                buttonPickBot.style.backgroundColor = "#3A0272";
+                buttonPickBot.style.color = "#EFCEFF";
+                playerPicture.style.backgroundColor = "#3A0272";
+                playerPicture.src = "src/assets/img/card-background-dark-purple.png";
+            }
+        }
+
+        if (index === 1) {
+            namePlayer.style.color = "#EFCEFF";
+            if (nameCharacter) {
+                nameCharacter.style.color = "#EFCEFF";
+                navigationArrowLeft.style.borderRight = "12px solid #EFCEFF";
+                navigationArrowRight.style.borderLeft = "12px solid #EFCEFF";
+                playerPicture.src = "src/assets/img/kandinsky-download-1719222086261%201.svg";
+            }
+            if (buttonPickBot) {
+                buttonPickBot.style.backgroundColor = "#8323B0";
+                buttonPickBot.style.color = "#EFCEFF";
+                playerPicture.style.backgroundColor = "#8323B0";
+                playerPicture.src = "src/assets/img/card-background-purple.png";
+            }
+        }
+
+        if (index === 2) {
+            cardBackground.style.backgroundColor = "#FBFF35";
+            namePlayer.style.color = "#818414";
+            if (nameCharacter) {
+                nameCharacter.style.color = "#818414";
+                navigationArrowLeft.style.borderRight  = "12px solid #818414";
+                navigationArrowRight.style.borderLeft = "12px solid #818414";
+                playerPicture.src = "src/assets/img/kandinsky-download-1719222086261%201.svg";
+            }
+            if (buttonPickBot) {
+                buttonPickBot.style.backgroundColor = "#FDFFA3";
+                buttonPickBot.style.color = "#AAAC1D";
+                playerPicture.style.backgroundColor = "#FDFFA3";
+                playerPicture.src = "src/assets/img/card-background-yellow.png";
+            }
+        }
+
+        if (index === 3) {
+            cardBackground.style.backgroundColor = "#FFC635";
+            namePlayer.style.color = "#a18122";
+            if (nameCharacter) {
+                nameCharacter.style.color = "#a18122";
+                navigationArrowLeft.style.borderRight = "12px solid #a18122";
+                navigationArrowRight.style.borderLeft = "12px solid #a18122";
+                playerPicture.src = "src/assets/img/kandinsky-download-1719222086261%201.svg";
+            }
+            if (buttonPickBot) {
+                buttonPickBot.style.backgroundColor = "#FFDA7B"
+                buttonPickBot.style.color = "#CFA333";
+                playerPicture.style.backgroundColor = "#FFDA7B";
+                playerPicture.src = "src/assets/img/card-background-dark-yellow.png";
+            }
+        }
+
+    } else {
+        console.error('Invalid index:', index);
+    }
+}
+
+function addBot(button) {
+    const waitingCard = button.closest('.player');
     const botTemplate = document.getElementById('bot-template');
-    const botCard= botTemplate.content.cloneNode(true);
-    waitingCard.replaceWith(botCard);
+    if (botTemplate && waitingCard) {
+        const botCard = botTemplate.content.cloneNode(true);
+        waitingCard.replaceWith(botCard);
+    }
+
+    setTimeout(() => {
+        setStyleCard(0);
+        setStyleCard(1);
+        setStyleCard(2);
+        setStyleCard(3);
+    }, 0);
 }
 
 function loadToMainPageLink() {
@@ -73,8 +173,17 @@ function loadToMainPageLink() {
     document.body.appendChild(toMainPage);
 }
 
+function initCardEventListeners() {
+    buttonGetBot = document.querySelectorAll('.player-get-bot'); // выбираем все кнопки
+
+    buttonGetBot.forEach(function(button) {
+        button.addEventListener('click', function() {
+            addBot(button);
+        });
+    });
+}
+
 function initEventListeners() {
-    nameGame = document.getElementById('central-part-menu');
     centralPartMenu = document.getElementById('central-part-menu');
     buttonBot = document.getElementById('button-bot');
     buttonPlayer = document.getElementById('button-player');
@@ -82,17 +191,14 @@ function initEventListeners() {
     button2vs2 = document.getElementById('button-2vs2');
     buttonLobby = document.getElementById('button-lobby');
     buttonConnect = document.getElementById('button-connect');
-    buttonGetBot = document.getElementById('get-bot');
-    cardBox = document.getElementById('card-container');
     buttonPlay = document.getElementById('button-play');
     buttonMenu = document.getElementById('button-menu');
     buttonLeave = document.getElementById('button-leave');
     inputRoomId = document.getElementById('input-code');
-    waitingCard = document.getElementById('waiting-template');
+    cardBox = document.getElementById('card-container');
 
     if (buttonBot) {
         buttonBot.addEventListener('click', () => {
-            // centralPartMenu.classList.add("central-part__menu-animate-out");
             setTimeout(() => transitionToPage("with_bot.html"), 0);
         });
     }
@@ -115,22 +221,18 @@ function initEventListeners() {
             socket.on('joinedRoom', (roomId) => {
                 globalRoomId = roomId;
                 transitionToPage("lobby.html");
-            })
-            // socket.on('roomCreated', () => {
-            //     socket.emit('joinRoom');
-            // })
+            });
         });
     }
 
     if (buttonConnect) {
         buttonConnect.addEventListener('click', () => {
-            inputRoomId.classList.toggle("input-for-code")
+            inputRoomId.classList.toggle("input-for-code");
         });
     }
 
     if (inputRoomId) {
-        inputRoomId.addEventListener('input', (event) => {
-            // Удаляем все нецифровые символы
+        inputRoomId.addEventListener('input', () => {
             inputRoomId.value = inputRoomId.value.replace(/\D/g, '');
             if (inputRoomId.value.length === 6) {
                 let codeRoom = document.getElementById('input-code').value;
@@ -138,11 +240,10 @@ function initEventListeners() {
                 socket.on('joinedRoom', (roomId) => {
                     globalRoomId = roomId;
                     transitionToPage("lobby.html");
-                    console.log('transition');
-                })
+                });
                 socket.on('wrongId', () => {
                     // добавить обработку несуществующего айди комнаты
-                })
+                });
             }
             if (inputRoomId.value.length > 6) {
                 inputRoomId.value = inputRoomId.value.slice(0, 6);
@@ -156,9 +257,14 @@ function initEventListeners() {
         loadCard('waiting-template');
         loadCard('waiting-template');
 
-        if (waitingCard) {
-            buttonGetBot.addEventListener('click', () => {addBot(this)});
-        }
+        setTimeout(() => {
+            setStyleCard(0);
+            setStyleCard(1);
+            setStyleCard(2);
+            setStyleCard(3);
+        }, 0);
+
+        initCardEventListeners(); // Инициализируем события для добавленных карточек
     }
 
     if (buttonPlay) {
@@ -166,7 +272,7 @@ function initEventListeners() {
             buttonPlay.textContent = 'READY';
             setTimeout(() => {
                 window.location.href = "game.html";
-            }, 1000)
+            }, 1000);
         });
     }
 
@@ -189,6 +295,3 @@ loadHTML('menu.html', (html) => {
     loadToMainPageLink();
     initEventListeners();
 });
-
-
-
